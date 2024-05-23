a0c68bd0c98067d3ce053ef1b8f066f83a84c88d
Sarthak Jha <jhasarthak9@gmail.com>
2024-05-23 06:44:02 +0530

    bruh

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# codeforces-gaslighter" 
+"# codeforces-gaslighter"
+
+<h2>Usage</h2>
+clone the repo using CLI(git clone)->npm install to install all the packages and dependencies->change the username and password in the script(lines 9 and 11)->node cfbot.js to see it working.
+
+<b>NOTE: </b>To change the number of iterations, change the while loop condition by adding an iterating variable for the number of times you want to iterate.
---
+++ cfbot.js
@@ -0,0 +1,108 @@
+const puppeteer = require("puppeteer");
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://codeforces.com/");
+  await page.click('a[href="/enter?back=%2F"]');
+  await new Promise((r) => setTimeout(r, 1000));
+
+  await page.type("#handleOrEmail", "sargeDevX");
+  await new Promise((r) => setTimeout(r, 2000));
+  await page.type("#password", "sarthak2004cf");
+  await page.click(".submit");
+
+  // await page.focus(".notice");
+  // await page.waitForSelector(".handleBox");
+  // await page.type(".handleBox", "rishijha00007");
+  // await page.click(
+  //   "#sidebar > div:nth-child(9) > form > div:nth-child(3) > input[type=submit]"
+  // );
+  await new Promise((r) => setTimeout(r, 4000));
+  console.log("after waiting");
+  while (true) {
+    await page.click('a[href="/problemset"]');
+    await new Promise((r) => setTimeout(r, 4000));
+    await page.click(
+      "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td.dark.right > a"
+    );
+    await new Promise((r) => setTimeout(r, 2000));
+    // await page.click("#verdictName");
+    // await new Promise((r) => setTimeout(r, 500));
+    await page.waitForSelector("#verdictName");
+
+    await page.click("#verdictName");
+    await new Promise((r) => setTimeout(r, 1000));
+    // await page.click("#verdictName > option:nth-child(2)");
+    await page.evaluate(() => {
+      const option = document.querySelector("#verdictName");
+      option.value = "OK";
+    });
+    await page.waitForSelector("#programTypeForInvoker");
+
+    await page.click("#programTypeForInvoker");
+    await new Promise((r) => setTimeout(r, 1000));
+    // await page.click("#verdictName > option:nth-child(2)");
+    await page.evaluate(() => {
+      const option = document.querySelector("#programTypeForInvoker");
+      option.value = "cpp.g++17";
+    });
+    await new Promise((r) => setTimeout(r, 500));
+    await page.click(
+      "#sidebar > div > div:nth-child(3) > form > div:nth-child(4) > input[type=submit]:nth-child(1)"
+    );
+    await new Promise((r) => setTimeout(r, 4000));
+    // await page.click(
+    //   "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td.id-cell.dark.left > a"
+    // );
+    await page.click(
+      "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td.id-cell.dark.left > a"
+    );
+    await page.waitForSelector("#facebox > div > div > div > span > a");
+    await new Promise((r) => setTimeout(r, 2000));
+    await page.click("#facebox > div > div > div > span > a");
+    await new Promise((r) => setTimeout(r, 2000));
+    await page.click(".source-copier");
+    await new Promise((r) => setTimeout(r, 500));
+    await page.waitForSelector(
+      "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(3) > a"
+    );
+    await new Promise((r) => setTimeout(r, 1000));
+
+    await page.click(
+      "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(3) > a"
+    );
+    await page.click(
+      "#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(3) > a"
+    );
+    await new Promise((r) => setTimeout(r, 500));
+    let quesNum = await page.evaluate(() => {
+      let currentUrl = window.location.href;
+      let lastChar = currentUrl.charAt(currentUrl.length - 1);
+      return lastChar;
+    });
+    console.log(quesNum);
+    await new Promise((r) => setTimeout(r, 8000));
+    await page.click(
+      "#pageContent > div.second-level-menu > ul > li:nth-child(3) > a"
+    );
+    await new Promise((r) => setTimeout(r, 3000));
+    await page.click(
+      "#pageContent > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > label > select"
+    );
+    await new Promise((r) => setTimeout(r, 1000));
+    await page.evaluate((quesNum) => {
+      const option = document.querySelector(
+        "#pageContent > form > table > tbody > tr:nth-child(1) > td:nth-child(2) > label > select"
+      );
+      option.value = quesNum;
+    }, quesNum);
+    await page.click("#editor > div.ace_scroller > div");
+    await page.click("#editor > div.ace_scroller > div");
+    await page.keyboard.down("Control");
+    await page.keyboard.press("V");
+    await page.keyboard.up("Control");
+    await new Promise((r) => setTimeout(r, 1000));
+    await page.click("#singlePageSubmitButton");
+    await new Promise((r) => setTimeout(r, 5000));
+  }
+})();
